ca5fd23ef156f52058d52d7ed71667582616d8e7
thanhtiensinh <129504816+thanhtiensinh@users.noreply.github.com>
2025-12-30 13:28:40 +0700

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
     cors({
-        origin: 'http://localhost:5173',
+        origin: 'https://hpu-library-fondend1.onrender.com',
         credentials: true,
     }),
 );
