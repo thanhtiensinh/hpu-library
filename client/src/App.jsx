07c7b226c6b953810a00f8176d32ba9f5a465e0e
thanhtiensinh <129504816+thanhtiensinh@users.noreply.github.com>
2025-12-31 08:17:13 +0700

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Header from './Components/Header/Header';
 import HomePage from './Components/HomePage/HomePage';
 import Footer from './Components/Footer/Footer';
@@ -11,6 +12,13 @@ function App() {
             </header>
 
             <main className="w-full flex justify-center mt-3">
+                <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/index.html" element={<Navigate to="/" replace />} />
+          {/* Ví dụ: <Route path="/login" element={<Login />} /> */}
+          {/* Ví dụ: <Route path="/cart" element={<CartUser />} /> */}
+          <Route path="*" element={<div className="text-center">Trang không tồn tại hoặc bạn không có quyền truy cập</div>} />
+        </Routes>
                 <HomePage />
             </main>
 
